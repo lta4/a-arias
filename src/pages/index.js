cad78ab161934eeddaa8d196353220a7f1fbd449
Andrew <andrew.mk4@yahoo.com>
2021-11-11 09:35:45 -0800

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 // import { StaticImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
@@ -8,23 +8,28 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to Andrew's Portfolio.</p>
-    <p>Now go build something great.</p>
-    {/* <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["auto", "webp", "avif"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    /> */}
-    {/* <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
-    </p> */}
+    <div className="container">
+      <div className="statementContainer">
+        <h1>Hello, I'm Andrew Arias.</h1>
+          <p>Welcome to Andrew's Portfolio.</p>
+          <p>Now go build something great.</p>
+          <button>View my work</button>
+          {/* <StaticImage
+            src="../images/gatsby-astronaut.png"
+            width={300}
+            quality={95}
+            formats={["auto", "webp", "avif"]}
+            alt="A Gatsby astronaut"
+            style={{ marginBottom: `1.45rem` }}
+          /> */}
+          {/* <p>
+            <Link to="/page-2/">Go to page 2</Link> <br />
+            <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
+            <Link to="/using-ssr">Go to "Using SSR"</Link> <br />
+            <Link to="/using-dsg">Go to "Using DSG"</Link>
+          </p> */}
+      </div>
+    </div>
   </Layout>
 )
 

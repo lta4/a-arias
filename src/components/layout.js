76377a62fabdbfa0baca339ef4a3c4ecb9c0e342
Andrew <andrew.mk4@yahoo.com>
2021-11-12 11:10:10 -0800

    social icons made

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,9 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
-import Footer from "./footer"
+// import Footer from "./footer"
+import { FaGithubAlt, FaEnvelope, FaLinkedin } from "react-icons/fa"
 import "./layout.css"
 
 const Layout = ({ children }) => {
@@ -20,12 +20,21 @@ const Layout = ({ children }) => {
   return (
     <div className="flexThisBox">
       <main>{children}</main>
-      <footer
-        style={{
-          marginTop: `2rem`,
-        }}
-      >
-        <Footer />
+      <footer>
+        <div className="footer__socials">
+          <a href="https://github.com/lta4">
+            <FaGithubAlt />
+          </a>
+          <a href="mailto:andrew4ld@gmail.com">
+            <FaEnvelope />
+          </a>
+          <a href="https://linkedin.com/in/andrewarias4">
+            <FaLinkedin />
+          </a>
+        </div>
+        <div className="footer__attributions">
+
+        </div>
         © {new Date().getFullYear()}, Built with love by
         {` `}
         <a href="">Andrew Arias</a>

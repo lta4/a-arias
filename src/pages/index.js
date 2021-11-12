f9cd1c604d276fb51369da8c282d0b0e9784ddb8
Andrew <andrew.mk4@yahoo.com>
2021-11-12 02:19:40 -0800

    header and projects updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,28 +19,34 @@ const IndexPage = () => (
     </header>
     <section className="projects">
       <div className="projects__card">
-        <div>
+        <div className="projects__card__description">
           <h2>Anxiety App</h2>
-            <p>My final Capstone Project. An app built to help anyone who has anxiety.</p>
+          <p>My final Capstone Project. An app built to help anyone who has anxiety.</p>
         </div>
+        <div className="projects__card__links">
           <a href="https://github.com/lta4/capstone-anxiety-app.git">Repo</a>
           <a href="https://capstone-anxiety-app.netlify.app">Project</a>
+        </div>
       </div>
       <div className="projects__card">
-        <div>
+        <div className="projects__card__description">
           <h2>PC Builder</h2>
-            <p>A group project made to sort and compile your personal computer.</p>
+          <p>A group project made to sort and compile your personal computer.</p>
         </div>
+        <div className="projects__card__links">
           <a href="https://github.com/MrChaco007/PC-Builder-Front.git">Repo</a>
           <a href="https://pc-builder.netlify.app">Project</a>
+        </div>
       </div>
       <div className="projects__card">
-        <div>
+        <div className="projects__card__description">
           <h2>moviFi</h2>
-            <p>An app created to find any movie out there. Descriptions included.</p>
+          <p>An app created to find any movie out there. Descriptions included.</p>
         </div>
+        <div className="projects__card__links">
           <a href="https://movifi.netlify.app">Repo</a>
           <a href="https://github.com/lta4/moviFi.git">Project</a>
+        </div>
       </div>
     </section>
   </Layout>

ced17061db51463ec9372d83492e59ef2e25fa06
Andrew <andrew.mk4@yahoo.com>
2021-11-19 01:51:40 -0800

    form added

--- src/pages/index.js
+++ src/pages/index.js
@@ -110,6 +110,30 @@ const IndexPage = () => (
         </div>
       </div>
     </section>
+    <section>
+      <div className="form">
+        <form method="post" action="#">
+          <label>
+            Name
+            <input type="text" name="name" id="name" />
+          </label>
+          <label>
+            Email
+            <input type="email" name="email" id="email" />
+          </label>
+          <label>
+            Subject
+            <input type="text" name="subject" id="subject" />
+          </label>
+          <label>
+            Message
+            <textarea name="message" id="message" rows="5" />
+          </label>
+            <button type="submit">Send</button>
+            <input type="reset" value="Clear" />
+          </form>
+        </div>
+    </section>
   </Layout>
 )
 
